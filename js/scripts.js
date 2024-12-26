42baedc1283b03267088b4e2b8549a871dad5864
ptd <dung.phank24@hcmut.edu.vn>
2024-12-26 22:30:07 +0700

    feat: layout navigate

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,12 @@ function showSection(sectionId) {
         selectedSection.style.display = 'block';
     }
 }
+
+
+function setActive(section) {
+    const navItems = document.querySelectorAll('.custom-nav-item');
+    navItems.forEach(item => {
+        item.classList.remove('active');
+    });
+    document.querySelector(`.custom-nav-item a[onclick*="${section}"]`).parentElement.classList.add('active');
+}
